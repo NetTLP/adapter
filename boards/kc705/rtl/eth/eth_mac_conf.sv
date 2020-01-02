05a3861238a6ffe71eb1b09dd81b7eff10c8fa03
sora <sora@haeena.net>
2020-01-02 19:38:55 +0900

    refactoring

--- boards/kc705/rtl/eth/eth_mac_conf.sv
+++ boards/kc705/rtl/eth/eth_mac_conf.sv
@@ -1,50 +1,48 @@
 module eth_mac_conf #(
-	parameter SRC_MAC = 48'h001122334455
+	parameter PAUSE_FRAME_SOURCE_MAC = 48'h001122334455,
+	parameter MTU_SIZE = 15'd1518
 )(
 	output wire [79:0] mac_tx_configuration_vector,
 	output wire [79:0] mac_rx_configuration_vector
 );
 
-    // TX Configuration
-    assign mac_tx_configuration_vector [79:32] = SRC_MAC;
-    assign mac_tx_configuration_vector [30:16] = 1518;
-    assign mac_tx_configuration_vector [14] = 0;
-    assign mac_tx_configuration_vector [10] = 0;  // DIC enable
-    assign mac_tx_configuration_vector [9] = 0;
-    assign mac_tx_configuration_vector [8] = 0;
-    assign mac_tx_configuration_vector [7] = 0;
-    assign mac_tx_configuration_vector [5] = 0;
-    assign mac_tx_configuration_vector [4] = 1;  // jumbo frame enable
-    assign mac_tx_configuration_vector [3] = 0;
-    assign mac_tx_configuration_vector [2] = 1;  // VLAN enable
-    assign mac_tx_configuration_vector [1] = 1;  // TX enable
-    assign mac_tx_configuration_vector [0] = 0;
+	// TX
+	assign mac_tx_configuration_vector[79:32] = PAUSE_FRAME_SOURCE_MAC;
+	assign mac_tx_configuration_vector[31] = 1'b0;
+	assign mac_tx_configuration_vector[30:16] = MTU_SIZE;
+	assign mac_tx_configuration_vector[15] = 1'b0;
+	assign mac_tx_configuration_vector[14] = 1'b0;
+	assign mac_tx_configuration_vector[13:11] = 3'b0;
+	assign mac_tx_configuration_vector[10] = 1'b0;  // DIC enable
+	assign mac_tx_configuration_vector[ 9] = 1'b0;
+	assign mac_tx_configuration_vector[ 8] = 1'b0;
+	assign mac_tx_configuration_vector[ 7] = 1'b0;
+	assign mac_tx_configuration_vector[ 6] = 1'b0;
+	assign mac_tx_configuration_vector[ 5] = 1'b0;
+	assign mac_tx_configuration_vector[ 4] = 1'b1;  // jumbo frame enable
+	assign mac_tx_configuration_vector[ 3] = 1'b0;
+	assign mac_tx_configuration_vector[ 2] = 1'b0;  // VLAN enable
+	assign mac_tx_configuration_vector[ 1] = 1'b1;  // TX enable
+	assign mac_tx_configuration_vector[ 0] = 1'b0;
 
-    // RX Configuration
-    assign mac_rx_configuration_vector [79:32] = SRC_MAC;
-    assign mac_rx_configuration_vector [30:16] = 1518;
-    assign mac_rx_configuration_vector [14] = 0;
-    assign mac_rx_configuration_vector [10] = 0;
-    assign mac_rx_configuration_vector [9] = 1;  // frame length check disable
-    assign mac_rx_configuration_vector [8] = 1;  // Length/Type error check disable
-    assign mac_rx_configuration_vector [7] = 0;
-    assign mac_rx_configuration_vector [5] = 0;
-    assign mac_rx_configuration_vector [4] = 1;  // jumbo frame enable
-    assign mac_rx_configuration_vector [3] = 0;
-    assign mac_rx_configuration_vector [2] = 1;  // VLAN enable
-    assign mac_rx_configuration_vector [1] = 1;  // RX enable
-    assign mac_rx_configuration_vector [0] = 0;
-
-    //Unused bits to 0
-    assign mac_tx_configuration_vector [31] = 1'b0;
-    assign mac_tx_configuration_vector [15] = 1'b0;
-    assign mac_tx_configuration_vector [13:11] = 3'b0;
-    assign mac_tx_configuration_vector [6] = 1'b0; 
-
-    assign mac_rx_configuration_vector [31] = 1'b0;
-    assign mac_rx_configuration_vector [15] = 1'b0;
-    assign mac_rx_configuration_vector [13:11] = 3'b0;
-    assign mac_rx_configuration_vector [6] = 1'b0; 
+	// RX
+	assign mac_rx_configuration_vector[79:32] = PAUSE_FRAME_SOURCE_MAC;
+	assign mac_rx_configuration_vector[31] = 1'b0;
+	assign mac_rx_configuration_vector[30:16] = MTU_SIZE;
+	assign mac_rx_configuration_vector[15] = 1'b0;
+	assign mac_rx_configuration_vector[14] = 1'b0;
+	assign mac_rx_configuration_vector[13:11] = 3'b0;
+	assign mac_rx_configuration_vector[10] = 1'b0;
+	assign mac_rx_configuration_vector[ 9] = 1'b1;  // frame length check disable
+	assign mac_rx_configuration_vector[ 8] = 1'b1;  // Length/Type error check disable
+	assign mac_rx_configuration_vector[ 7] = 1'b0;
+	assign mac_rx_configuration_vector[ 6] = 1'b0;
+	assign mac_rx_configuration_vector[ 5] = 1'b0;
+	assign mac_rx_configuration_vector[ 4] = 1'b1;  // jumbo frame enable
+	assign mac_rx_configuration_vector[ 3] = 1'b0;
+	assign mac_rx_configuration_vector[ 2] = 1'b0;  // VLAN enable
+	assign mac_rx_configuration_vector[ 1] = 1'b1;  // RX enable
+	assign mac_rx_configuration_vector[ 0] = 1'b0;
 
 endmodule
 
--- boards/kc705/rtl/eth/eth_pcspma_conf.sv
+++ boards/kc705/rtl/eth/eth_pcspma_conf.sv
@@ -1,5 +1,5 @@
 module eth_pcspma_conf (
-    output wire [535:0] pcspma_configuration_vector
+	output wire [535:0] pcspma_configuration_vector
 );
 	assign pcspma_configuration_vector = 536'b0;
 
